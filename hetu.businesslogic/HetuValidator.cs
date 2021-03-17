44bf960fcad7745c14b0b001aac12e27c423ac53
Katri Hautala <katri@kyrodistillery.com>
2021-03-17 17:48:58 +0200

    Initial commit

---
+++ hetu.businesslogic/HetuValidator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace hetu.businesslogic
+{
+    public class HetuValidator
+    {
+    }
+}
---
+++ hetu.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hetu.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
